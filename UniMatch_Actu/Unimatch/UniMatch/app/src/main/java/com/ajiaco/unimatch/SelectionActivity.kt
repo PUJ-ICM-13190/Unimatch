2f0e688f98b8e555b5719c8c41728b94d496be1a
Nicocamachoa <camachoa.nicolas@javeriana.edu.co>
2024-10-14 17:47:16 -0500

    TRABAJAR AQUI

---
+++ UniMatch_Actu/Unimatch/UniMatch/app/src/main/java/com/ajiaco/unimatch/SelectionActivity.kt
@@ -0,0 +1,24 @@
+package com.ajiaco.unimatch.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ajiaco.unimatch.databinding.ActivitySelectionBinding
+
+class SelectionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySelectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java)) // Redirige a la pantalla de Iniciar Sesión
+        }
+
+        binding.buttonRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java)) // Redirige a la pantalla de Registro
+        }
+    }
+}
